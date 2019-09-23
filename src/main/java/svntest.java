e9ca85ede5274aea485f0515fd4605c21038e5a6
wxx <wxxwilson@163.com>
2019-09-23 16:19:58 +0800

    test for github

--- src/main/java/svntest.java
+++ src/main/java/svntest.java
@@ -2,5 +2,6 @@ public class svntest {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("hello word");
+        System.out.println("hello word3");
     }
 }
